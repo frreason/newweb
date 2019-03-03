d672da18858f9b46b2b6cf7d3d8a840f870e0f4f
frreason <13417658139@163.com>
2019-03-03 15:43:09 +0800

    secondly

--- webserver/main.go
+++ webserver/main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 )
 
-func firstPage(w http.ResponseWriter,r *http.Request){
-	io.WriteString(w,"<h1> helo,this is my first page! </h1>")
+func Print1to20() int {
+	res := 0
+	for i := 1; i <= 20; i++ {
+		res += i
+	}
+	return res
 }
 
-func main(){
-	http.HandleFunc("/",firstPage)
-	http.ListenAndServe(":8000",nil)
+func firstPage(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "<h1> helo,this is my first page! </h1>")
+	io.WriteString(w, "<h1> hahahahaha <h1>")
+}
 
+func main() {
+	fmt.Println("first page")
+	http.HandleFunc("/", firstPage)
+	http.ListenAndServe(":8000", nil)
 }
--- webserver/main_test.go
+++ webserver/main_test.go
@@ -1,15 +1,40 @@
 package main
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
 
-func TestPrint(t *testing.T){
+// func TestMain(m *testing.M) {
+// 	fmt.Println("testMain first")
+// 	m.Run()
+// }
+
+func testPrint(t *testing.T) {
+	//t.SkipNow()
 	res := Print1to20()
-	fmt.Println("hey")
-	if res != 210{
+	//res := 20
+	fmt.Println("hey1")
+	if res != 210 {
 		t.Errorf("Wrong result of Print1to20")
 	}
 }
 
+func testPrint2(t *testing.T) {
+	res := Print1to20()
+	res++
+	if res != 211 {
+		t.Errorf("Test Print2 failed")
+	}
+
+}
+func TestAll(t *testing.T) {
+	t.Run("TestPrint", testPrint)
+	t.Run("TestPrint2", testPrint2)
+}
+
+func BenchmarkAll(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Print1to20()
+	}
+}
